cbe6a0733ea73d982458b2fde66859db10ac32b2
Pinky Parsons <pinky.parsons@passportinc.com>
2019-03-19 12:44:15 -0400

    fixed an issue with the path in appjs

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 // const express = require('express');
-import express from 'express';
-import fs from 'fs';
+const express = require('express');
+const fs = require('fs');
 
-const _ = fs.readFile('../ptc-76-ticketed-pinky.json', (err, data) => {
+const _ = fs.readFile('./ptc-76-ticketed-pinky.json', (err, data) => {
   if (err) throw err;
   const ptcdata = JSON.parse(data);
   console.log(ptcdata);
